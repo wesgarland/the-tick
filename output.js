4be9a1d344f51957540e5224447d19a31892c342
Wes Garland <wes@page.ca>
2021-08-12 07:56:31 -0400

    output.js - limit max lines displayed in the textarea

--- output.js
+++ output.js
@@ -1,10 +1,10 @@
 module.declare((require, exports, module) => {
-  function $(s) {
-    return document.querySelector(s);
-  }
+  exports.maxLines = 10000;
+  exports.lines    = [];
 
   exports.log = function output$$log()
   {
+    const textarea = document.querySelector('#output > TEXTAREA');
     var outs = [];
 
     for (let arg of arguments)
@@ -17,9 +17,16 @@ module.declare((require, exports, module) => {
 	outs.push(arg + '');
     }
 
-    if (outs.length)
-      $('#output > TEXTAREA').value += outs.join(' ') + '\n';      
-      //      $('#output > TEXTAREA').textContent += outs.join(' ') + '\n';
+    if (!outs.length)
+      return;
+
+    exports.lines.push(outs.join(' '));
+    if (exports.lines.length > exports.maxLines)
+      exports.lines.splice(0, 1);
+    
+    textarea.value = exports.lines.join('\n');
+    textarea.scrollTop = textarea.scrollHeight;
   }
 
+  exports.debug = exports.notice = exports.warn = exports.log;
 });
